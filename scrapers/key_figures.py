df773ff7a3ed31d03cbafcd733cab3d12278aec0
Mike <rans@un.org>
2022-09-22 10:30:15 +1200

    Get key figures from gsheet

--- scrapers/key_figures.py
+++ scrapers/key_figures.py
@@ -14,20 +14,18 @@ class KeyFigures(BaseScraper):
             {
                 "national": (
                     (
-                        "FundingTimeline",
                         "RequiredFunding",
                         "Funding",
                         "PercentFunded",
-                        "Affected",
+                        "InNeed",
                         "Targeted",
                         "Reached",
                     ),
                     (
-                        "#date+funding",
                         "#value+funding+required+usd",
                         "#value+funding+total+usd",
                         "#value+funding+pct",
-                        "#affected",
+                        "#inneed",
                         "#targeted",
                         "#reached",
                     ),
@@ -43,13 +41,12 @@ class KeyFigures(BaseScraper):
             url, headers=1, dict_form=True, format="csv"
         )
         rows = list(iterator)
-        timelines = self.get_values("national")[0]
-        requirements = self.get_values("national")[1]
-        funding = self.get_values("national")[2]
-        percentage = self.get_values("national")[3]
-        affected = self.get_values("national")[4]
-        targeted = self.get_values("national")[5]
-        reached = self.get_values("national")[6]
+        requirements = self.get_values("national")[0]
+        funding = self.get_values("national")[1]
+        percentage = self.get_values("national")[2]
+        affected = self.get_values("national")[3]
+        targeted = self.get_values("national")[4]
+        reached = self.get_values("national")[5]
         reader = self.get_reader("hdx")
         for ds_row in rows:
             countryiso3 = ds_row["Country ISO"]
@@ -62,24 +59,23 @@ class KeyFigures(BaseScraper):
                 logger.warning(f"No Key Figures data for {countryiso3}.")
                 continue
             resource = dataset.get_resource()
-            headers, iterator = reader.get_tabular_rows(
-                resource["url"], dict_form=True, format="xlsx"
-            )
-            for row in iterator:
-                timelines[countryiso3] = row["Funding timeline"]
+            data = reader.read_hxl_resource(countryiso3, resource, "Key Figures data")
+            if data is None:
+                continue
+            for row in data:
                 requirements[countryiso3] = number_format(
-                    row["Total Fund Requirement"], format="%.0f"
+                    row.get("#value+funding+required"), format="%.0f"
                 )
-                funding[countryiso3] = number_format(row["Funded"], format="%.0f")
-                percentage[countryiso3] = number_format(row["Funded %"], format="%.2f")
+                funding[countryiso3] = number_format(row.get("#value+funding+total"), format="%.0f")
+                percentage[countryiso3] = number_format(row.get("#value+funding+pct"), format="%.2f")
                 affected[countryiso3] = number_format(
-                    row["Total Affected"], format="%.0f"
+                    row.get("#inneed"), format="%.0f"
                 )
                 targeted[countryiso3] = number_format(
-                    row["Total Targeted"], format="%.0f"
+                    row.get("#targeted"), format="%.0f"
                 )
                 reached[countryiso3] = number_format(
-                    row["Total Reached"], format="%.0f"
+                    row.get("#reached"), format="%.0f"
                 )
 
         self.datasetinfo["source_date"] = self.today
